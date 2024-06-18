a0fea063aaee02d643e02c937cd3ed7f5a86cc47
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2024-06-18 20:00:30 -0300

    :sparkles: feat: shadcn setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,10 @@ export default defineConfig({
     strictPort: true,
     host: true,
     origin: "http://0.0.0.0:5173",
+  },
+  resolve: {
+    alias: {
+      "@presentation": path.resolve(__dirname, "./src/presentation"),
+    },
   },
 });
