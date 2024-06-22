afde17547cd6775c481fdded7210dd210f5f8429
Vinícius Guedes <comercial.viniciusguedes@gmail.com>
2024-06-22 01:16:59 -0300

    :sparkles: feat: interaction between tabs and cards

--- src/store/menu/menu.reducer.ts
+++ src/store/menu/menu.reducer.ts
@@ -2,15 +2,18 @@ import { createSlice, Dispatch, PayloadAction } from "@reduxjs/toolkit";
 
 import { Menu } from "../../domain/models";
 import { usecasesFactory } from "../../main";
+import { UnknownAction } from "redux";
 
 export interface MenuState {
   menu: Menu | null;
   loading: boolean;
+  openSections: string[];
 }
 
 const initialState: MenuState = {
   menu: null,
   loading: false,
+  openSections: [],
 };
 
 const menuSlice = createSlice({
@@ -22,27 +25,37 @@ const menuSlice = createSlice({
     },
     fetchMenuSuccess: (state, action: PayloadAction<Menu>) => {
       state.menu = action.payload;
+      state.openSections = action.payload.sections.map((section) =>
+        section.id.toString()
+      );
       state.loading = false;
     },
     fetchMenuFailure: (state) => {
       state.loading = false;
     },
+    syncOpenSections: (state, action: PayloadAction<string[]>) => {
+      state.openSections = action.payload;
+    },
   },
 });
 
-export const { fetchMenuStart, fetchMenuSuccess, fetchMenuFailure } =
-  menuSlice.actions;
+export const {
+  fetchMenuStart,
+  fetchMenuSuccess,
+  fetchMenuFailure,
+  syncOpenSections,
+} = menuSlice.actions;
 
 export const menuReducer = menuSlice.reducer;
 
-export const fetchMenu = () => async (dispatch: Dispatch) => {
-  try {
-    dispatch(fetchMenuStart());
-    const getMenuUsecase = usecasesFactory.menu.makeGetMenuUsecase();
-    const data = await getMenuUsecase.execute();
-    dispatch(fetchMenuSuccess(data));
-  } catch (error) {
-    dispatch(fetchMenuFailure());
-    console.error("Erro ao buscar dados do menu:", error);
-  }
-};
+export const fetchMenu = () =>
+  (async (dispatch: Dispatch) => {
+    try {
+      dispatch(fetchMenuStart());
+      const getMenuUsecase = usecasesFactory.menu.makeGetMenuUsecase();
+      const data = await getMenuUsecase.execute();
+      dispatch(fetchMenuSuccess(data));
+    } catch (error) {
+      dispatch(fetchMenuFailure());
+    }
+  }) as unknown as UnknownAction;
